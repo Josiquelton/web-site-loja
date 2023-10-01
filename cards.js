8a0f60362d5bfbb7a65b0b502438d02e2ffdb6d8
Josiquelton Lima <josiqueltonls@gmail.com>
2023-10-01 09:02:23 -0300

    versao Inicial

---
+++ cards.js
@@ -0,0 +1,72 @@
+
+for(let i = 1; i<=1; i++){
+    alert("pagina de Produtos")
+}
+
+// função  usando querySelector para bordar 
+
+function addBlackBorder(id)
+{
+    element = document.querySelector("#" + id);
+    element.style.border = "5px solid black";
+    
+}
+function addBlackBorder(id)
+{
+    element = document.querySelector("#" + id);
+    element.style.border = "5px solid black";
+
+}
+function addBlackBorder(id)
+{
+    element = document.querySelector("#" + id);
+    element.style.border = "5px solid black";
+
+}
+function addBlackBorder(id)
+{
+    element = document.querySelector("#" + id);
+    element.style.border = "5px solid black";
+
+}
+function addBlackBorder(id)
+{
+    element = document.querySelector("#" + id);
+    element.style.border = "5px solid black";
+
+}
+function addBlackBorder(id)
+{
+    element = document.querySelector("#" + id);
+    element.style.border = "5px solid black";
+
+}
+function addBlackBorder(id)
+{
+    element = document.querySelector("#" + id);
+    element.style.border = "5px solid black";
+
+}
+function addBlackBorder(id)
+{
+    element = document.querySelector("#" + id);
+    element.style.border = "5px solid black";
+
+}
+
+addBlackBorder("areia");
+addBlackBorder("argila");
+addBlackBorder("brita");
+addBlackBorder("cimento");
+addBlackBorder("caixa");
+addBlackBorder("argamassa");
+addBlackBorder("mantas");
+addBlackBorder("drenagem");
+
+// função utilisando o querySelector para box-shadow
+
+function highlightCard(selector)
+{
+    var element = document.querySelector(selector);
+    element.classList.toggle("Card-highlight");
+}
